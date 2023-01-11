a76544db533a60afad157d04461b6143b82bf7b8
Cafe <Alepy@hotmail.com>
2023-01-11 23:13:53 +0100

    first_commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Calendar from 'react-calendar';
+
+function App() {
+
+  return (
+    <>
+
+      <Calendar> </Calendar>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/componentes/Calendar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Calendar.css';
+
+
+
+function Calendar() {
+
+    return (
+        <>
+
+
+        </>
+    );
+}
+
+export default Calendar;
